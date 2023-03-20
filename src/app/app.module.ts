554b824334647a32cc81ece70f5e966c742ad311
Houssam Bouazzaoui <houssam.bouazzaoui@student.hogent.be>
2023-03-21 00:12:31 +0100

    added firebase setup and toolbar navigation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,29 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { SharedModule } from './shared/shared.module';
+import { HomePageComponent } from './home-page/home-page.component';
+import { environment } from 'src/environments/environment';
+
+// Firebase imports
+import { AngularFireModule } from '@angular/fire/compat';
+import { AngularFireDatabaseModule } from '@angular/fire/compat/database';
+import { AngularFireAuthModule } from '@angular/fire/compat/auth';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomePageComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    SharedModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFireDatabaseModule,
+    AngularFireAuthModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/environments/environment.ts
@@ -0,0 +1,12 @@
+export const environment = {
+    production: false,
+    firebase: {
+        apiKey: "AIzaSyAGUphrYUaNO-6xznguUeoMrI4GdIwf6ow",
+        authDomain: "my-dev-kanban.firebaseapp.com",
+        projectId: "my-dev-kanban",
+        storageBucket: "my-dev-kanban.appspot.com",
+        messagingSenderId: "837154841658",
+        appId: "1:837154841658:web:4065495aa38a1deaac0103",
+        measurementId: "G-QLGFPPEYW5"
+    }
+}
